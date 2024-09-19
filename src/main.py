f223a9c0e5fd1954b8c992080b32701717724b53
Valeriya <Dzhavadova.v@yandex.ru>
2024-09-19 13:05:30 +0300

    add: time check

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio, time
 
 from database.db import create_db
 from services.service import SpimexParser
@@ -7,4 +7,7 @@ from services.service import SpimexParser
 if __name__ == "__main__":
     parser = SpimexParser(2024, 9)
     asyncio.run(create_db())
+    start_time = time.perf_counter()
     asyncio.run(parser.start())
+    elapsed = time.perf_counter() - start_time
+    print(f"done for: {round(elapsed, 2)} sec")
